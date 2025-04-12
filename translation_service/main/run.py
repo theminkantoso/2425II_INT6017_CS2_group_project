7a6e5687848c5b24b70406433da08720a28f96ef
phucpb <Phambaodu25072001@gmail.com>
2025-04-12 19:14:26 +0700

    feat: Translation service Rabbit

--- translation_service/main/run.py
+++ translation_service/main/run.py
@@ -1,15 +1,13 @@
 # worker.py
 import asyncio
 import json
-from pathlib import Path
 
 import aio_pika
 from _config import config
-from services import ocr_service
+from services import translation_service
 
 import logging
 
-SHARED_FOLDER = Path("/storage")
 logging.basicConfig(level=logging.INFO)
 
 
@@ -17,31 +15,32 @@ async def handle_message(message: aio_pika.IncomingMessage):
     async with message.process():
         data = message.body.decode()
         data = json.loads(data)
-        logging.info(f"OCR: Received message from RabbitMQ, processing content {data}")
+        logging.info(
+            f"Translation: Received message from RabbitMQ, processing content {data}"
+        )
         # your business logic here, use shared functions or DB access
-        file_path = data["file_path"]
-        text = await ocr_service.image_to_text(image_path=str(file_path))
+        text_to_translate = data["text_to_translate"]
+        translated_text = await translation_service.translate(text=text_to_translate)
 
-        # publish the result to RabbitMQ
-        await publish_message(message=json.dumps({"text_to_translate": text}))
+        await publish_message(message=json.dumps({"translated_text": translated_text}))
 
 
 async def publish_message(message: str):
-    connection = await aio_pika.connect_robust(config.RABBITMQ_CONNECTION)
+    connection = await aio_pika.connect_robust(config.RABBITMQ_QUEUE_PDF)
     async with connection:
         channel = await connection.channel()
         await channel.default_exchange.publish(
             aio_pika.Message(body=message.encode()),
-            routing_key=config.RABBITMQ_QUEUE_TRANSLATE,
+            routing_key=config.RABBITMQ_QUEUE_PDF,
         )
-        logging.info(f"OCR: Published message {message} to RabbitMQ")
+        logging.info(f"Translation: Published message {message} to RabbitMQ")
 
 
 async def main():
     connection = await aio_pika.connect_robust(config.RABBITMQ_CONNECTION)
     channel = await connection.channel()
-    queue = await channel.declare_queue(config.RABBITMQ_QUEUE, durable=True)
-    text = await queue.consume(handle_message)
+    queue = await channel.declare_queue(config.RABBITMQ_QUEUE_TRANSLATE, durable=True)
+    await queue.consume(handle_message)
 
     await asyncio.Future()  # keep the script alive
 
